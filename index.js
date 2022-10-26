770ce855d40ddedd551b2a788b9a5c7eab1152a7
SaifullaJubair <saifullajubair6@gmail.com>
2022-10-26 11:58:28 +0600

    course data theke id diye data paoa

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.get('/categories', (req, res) => {
    res.send(categories)
 })
 
+app.get('/course/:id', (req, res) => {
+   const id = req.params.id;
+   const selectedCourse = course.find(c => c._id === id)
+   res.send(selectedCourse)
+})
+
 
 app.listen(port, () => {
    console.log(`Course running on port ,${port}`);
