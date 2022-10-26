9636d2f3e15a8d69fd0a612b5938790835b94eb2
SaifullaJubair <saifullajubair6@gmail.com>
2022-10-26 10:01:29 +0600

    categories data display

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+const course = require('./data/course.json');
+
 app.use(cors());
 
-const course = require('./data/course.json')
 
 app.get('/', (req, res) => {
    res.send('Course Server is running')
 })
+
+app.get('/categories', (req, res) => {
+   res.send(categories)
+})
+
 
 app.listen(port, () => {
    console.log(`Course running on port ,${port}`);
